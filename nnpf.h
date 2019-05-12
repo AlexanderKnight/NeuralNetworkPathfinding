a166de032baab6204ee60bbccfb49ef530ddf568
Alexander Knight <Alexanderlknight@protonmail.com>
2019-05-12 16:11:56 -0400

    Add layer to header file

--- nnpf.h
+++ nnpf.h
@@ -5,6 +5,11 @@
 #include <vector>
 using namespace std;
 
+double fRand(double fMin, double fMax)
+{
+	    double f = (double)rand() / RAND_MAX;
+	        return fMin + f * (fMax - fMin);
+}
 
 class Domain{
 	int width, height;
@@ -17,6 +22,35 @@ class Domain{
 		const int get_loc_prop(int x,int y);
 };
 
+class Neuron{
+	vector<double> inputs;
+	vector<double> weights;
+	double bias, output;
+	public:
+		Neuron(vector<double> in,vector<double> w,double b);
+		const vector<double> get_inputs(void);
+		void set_inputs(vector<double> in);
+		const vector<double> get_weights(void);
+		void set_weights(vector<double> w):
+		const double get_bias(void);
+		void set_bias(double b);
+		double get_output(void);
+		void update_output(void);
+};
+
+class Layer{
+	vector<Neuron> neurons;
+	vector<double> outputs, inputs;
+	public:
+		Layer(vector<double> in, vector<Neuron> neu);
+		void set_inputs(vector<double> in);
+		const vector<double> get_outputs(void);
+		void update_outputs(void);
+};
+
+class Network{
+	vector<Layer> layers;
+	vector<double> outputs inputs;
 
 
 #endif
