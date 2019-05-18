23981dc255b80eb51979958fde02224b445825ba
Alexander Knight <Alexanderlknight@protonmail.com>
2019-05-18 18:00:43 -0400

    Removed class Domain to put back in dom.h

--- nnpf.h
+++ nnpf.h
@@ -33,22 +33,6 @@ vector<double> flatten_double_array(vector<vector<double>> v){
 	return new_v;
 }
 
-class Domain{
-	int width, height;
-	vector<vector<int>> domain;
-	public:
-		Domain(int w, int h);
-		Domain(int w, int h, int randChance);
-		const int get_width(void);
-		const int get_height(void);
-		void set_loc_prop(int x,int y,int s);
-		const int get_loc_prop(int x,int y);
-		void clear_loc(int x, int y);
-		void save_domain(string s);
-		void load_domain(string s);
-		const void print_domain(void);
-		void reset(void);
-};
 
 class Neuron{
 	vector<double> inputs;
