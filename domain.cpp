5efe2c705e23de5bf11f46d183534570b9a33aab
Alexander Knight <Alexanderlknight@protonmail.com>
2019-05-18 12:49:15 -0400

    Added more functions for domain, as well as saving and loading

--- domain.cpp
+++ domain.cpp
@@ -3,8 +3,10 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
-#include "nnpf.h"
+#include <fstream>
 using namespace std;
+
+#include "nnpf.h"
 
 /*
 	The domain class is build to create and handle the
@@ -27,6 +29,36 @@ Domain::Domain (int w, int h){
 	}
 }
 
+Domain::Domain (int w, int h, int randChance){
+	width = w;
+	height = h;
+	domain.resize(width);
+	for (int i=0;i<width;i++){
+		domain[i].resize(height);
+		for (int j=0;j<height;j++){
+			domain[i][j]=0;
+		}
+	}
+	srand(time(0));
+	for (int i=0;i<width;i++){
+		for (int j=0;j<height;j++){
+			if ((rand()%100+1)<randChance){
+				domain[i][j] = 1;
+			}
+		}
+	}
+	this->clear_loc(2,2);
+	this->clear_loc(width-2,height-2);
+	for (int i=0;i<width;i++){
+		domain[i][0] = 1;
+		domain[i][height-1] =1;
+	}
+	for (int j=0;j<height;j++){
+		domain[0][j] = 1;
+		domain[width-1][j] = 1;
+	}
+}
+
 const int Domain::get_width(void){
 	return width;
 }
@@ -43,30 +75,68 @@ const int Domain::get_loc_prop(int x,int y){
 	return domain[x][y];
 }
 
-/*int main(){
+void Domain::reset(void){
+	for(int i=0;i<width;i++){
+		for(int j=0;j<height;j++){
+			domain[i][j]=0;
+		}
+	}
+}
+
+void Domain::clear_loc(int x,int y){
+	domain[x-1][y-1] = 0;
+	domain[x][y-1] = 0;
+	domain[x-1][y] = 0;
+	domain[x][y] = 0;
+}
+
+void Domain::save_domain(string filename){
+	ofstream outfile(filename);
+	outfile << width << endl << height << endl;
+	for (int i=0;i<width;i++){
+		for (int j=0;j<height;j++){
+			outfile << domain[i][j] << " ";
+		}
+		outfile << endl;
+	}
+	outfile.close();
+}
+
+void Domain::load_domain(string filename){
+	ifstream infile(filename);
+	infile >> width;
+	infile >> height;
+	domain.resize(width);
+	for(int i=0;i<width;i++){
+		domain[i].resize(height);
+		for(int j=0;j<height;j++){
+			infile >> domain[i][j];
+		}
+	}
+	infile.close();
+}
+
+const void Domain::print_domain(void){
+	for (int i=0;i<width;i++){
+		for (int j=0;j<height;j++){
+			cout << this->get_loc_prop(i,j) << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
+
+
+int main(){
 	srand(time(0));
 	cout << "Random number is : " << rand() % 100+1 << endl;
 	int wid = 10;
 	int hei = 10;
-	Domain domain (wid,hei);
-	for (int i=0;i<wid;i++){
-		for (int j=0;j<hei;j++){
-			cout << domain.get_loc_prop(i,j) << " ";
-		}
-		cout << endl;
-	}
-	for (int i=0;i<wid;i++){
-		for (int j=0;j<hei;j++){
-			if ((rand()%100+1)<30){
-				domain.set_loc_prop(i,j,1);
-			}
-		}
-	}
-	cout << endl;
-	for (int i=0;i<wid;i++){
-		for (int j=0;j<hei;j++){
-			cout << domain.get_loc_prop(i,j) << " ";
-		}
-		cout << endl;
-	}
-}*/
+	int randChance = 30;
+	Domain domain (wid,hei,randChance);
+	domain.print_domain();
+	//domain.save_domain("TestDomain.txt");
+	domain.load_domain("TestDomain.txt");
+	domain.print_domain();
+}
--- nnpf.h
+++ nnpf.h
@@ -16,15 +16,22 @@ class Domain{
 	vector<vector<int>> domain;
 	public:
 		Domain(int w, int h);
+		Domain(int w, int h, int randChance);
 		const int get_width(void);
 		const int get_height(void);
 		void set_loc_prop(int x,int y,int s);
 		const int get_loc_prop(int x,int y);
+		void clear_loc(int x, int y);
+		void save_domain(string s);
+		void load_domain(string s);
+		const void print_domain(void);
+		void reset(void);
 };
 
 class Neuron{
 	vector<double> inputs;
 	vector<double> weights;
+	vector<double> weight_range, bias_range;
 	double bias, output;
 	public:
 		Neuron(vector<double> in,vector<double> w,double b);
@@ -32,26 +39,42 @@ class Neuron{
 		const vector<double> get_inputs(void);
 		void set_inputs(vector<double> in);
 		const vector<double> get_weights(void);
-		void set_weights(vector<double> w):
+		void set_weights(vector<double> w);
 		const double get_bias(void);
 		void set_bias(double b);
 		double get_output(void);
 		void update_output(void);
+		void mutate(void);
+		void reset(void);
 };
 
 class Layer{
 	vector<Neuron> neurons;
 	vector<double> outputs, inputs;
+	vector<double> weight_range, bias_range;
 	public:
 		Layer(vector<double> in, vector<Neuron> neu);
+		Layer(vector<double> in, int neuron_num);
 		void set_inputs(vector<double> in);
 		const vector<double> get_outputs(void);
 		void update_outputs(void);
+		void mutate(void);
+		void reset(void);
 };
 
 class Network{
 	vector<Layer> layers;
-	vector<double> outputs inputs;
+	vector<double> outputs, inputs;
+	vector<double> weight_range, bias_range;
+	public:
+		void mutate(void);
+		void reset(void);
+};
 
+class Seeker{
+	double score, x, y;
+	Network network;
+	vector<double> input, outputs;
+};
 
 #endif
